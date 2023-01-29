e9aee167ab00c4fb4dd0b36856b70e57cff7e09c
Thorsten Sideboard <thorsten.sideb0ard@gmail.com>
2023-01-28 16:05:45 -0800

    adding libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <sndfile.h>
 
+#include "BTrack.h"
+
 #include <filesystem>
 #include <iostream>
 // #include <fstream>
@@ -25,11 +27,17 @@ int main() {
   std::cout << "OPENED FILE YO - GOT CHANNEL:" << sf_info.channels
             << " aDN THIS MANY FRAMES:" << sf_info.frames << std::endl;
 
+  BTrack b;
+
   double local_buffer[kLocalBufferLen]{};
   sf_count_t count;
   while ((count = sf_read_double(snd_file, local_buffer, kLocalBufferLen)) >
          0) {
-    // for (int i = 0; i < count; i++) buffer.push_back(local_buffer[i]);
+    b.processAudioFrame(local_buffer);
+    if (b.beatDueInCurrentFrame())
+    {
+      std::cout << "BEAT YO!\n";
+    }
     std::cout << "Read:" << count << std::endl;
   }
   sf_close(snd_file);
