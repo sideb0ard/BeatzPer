717acd92ad57ac2ab4cfefb7605bb3de4c3568c9
Thorsten Sideboard <thorsten.sideb0ard@gmail.com>
2023-02-03 09:59:27 -0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <sndfile.h>
 
-#include "BTrack.h"
-
 #include <filesystem>
 #include <iostream>
+#include <numeric>
+
+#include "BTrack.h"
+
 // #include <fstream>
 namespace fs = std::filesystem;
 
@@ -11,10 +13,12 @@ constexpr int kLocalBufferLen = 1024;
 
 const std::string kSamplePath = "galcher.aiff";
 
-int main() {
+std::vector<double> GetBeatTimesFromAudioFile(std::string full_filename) {
   SNDFILE* snd_file;
   SF_INFO sf_info;
-  std::string full_filename = fs::current_path().string() + "/" + kSamplePath;
+  BTrack beat_tracker;
+
+  std::vector<double> beat_times_ms;
 
   sf_info.format = 0;
 
@@ -24,21 +28,34 @@ int main() {
               << sf_error(snd_file);
   }
 
-  std::cout << "OPENED FILE YO - GOT CHANNEL:" << sf_info.channels
-            << " aDN THIS MANY FRAMES:" << sf_info.frames << std::endl;
-
-  BTrack b;
-
+  double time_count_ms{0};
+  double time_per_frame_ms = 1000. / sf_info.samplerate;
   double local_buffer[kLocalBufferLen]{};
   sf_count_t count;
   while ((count = sf_read_double(snd_file, local_buffer, kLocalBufferLen)) >
          0) {
-    b.processAudioFrame(local_buffer);
-    if (b.beatDueInCurrentFrame())
-    {
-      std::cout << "BEAT YO!\n";
+    time_count_ms += time_per_frame_ms * (count / sf_info.channels);
+    beat_tracker.processAudioFrame(local_buffer);
+    if (beat_tracker.beatDueInCurrentFrame()) {
+      beat_times_ms.push_back(time_count_ms);
     }
-    std::cout << "Read:" << count << std::endl;
   }
   sf_close(snd_file);
+  return beat_times_ms;
+}
+
+int main() {
+  std::string full_filename = fs::current_path().string() + "/" + kSamplePath;
+  std::vector<double> beat_times_ms = GetBeatTimesFromAudioFile(full_filename);
+  std::vector<double> time_diffs;
+  for (int i = 1; i < beat_times_ms.size(); i++) {
+    double time_diff = beat_times_ms[i] - beat_times_ms[i - 1];
+    time_diffs.push_back(time_diff);
+    std::cout << "play galcher.aiff trim " << beat_times_ms[i - 1] / 1000.
+              << " =" << beat_times_ms[i] / 1000 << std::endl;
+  }
+  double avg_diff = std::accumulate(time_diffs.begin(), time_diffs.end(), 0) /
+                    time_diffs.size();
+  double bpm_estimate = 1000. * 60 / avg_diff;
+  std::cout << "BPM ESTIMATE:" << bpm_estimate << std::endl;
 }
